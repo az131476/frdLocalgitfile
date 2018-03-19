d9fb58cfa359492eca4afd7efe7ae5f33a93d52d
az131476 <1297953037@qq.com>
2018-03-19 18:07:23 +0800

    waveShape

---
+++ WaveShape/WaveShape/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WaveShape
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zGraph1 = new ZhengJuyin.UI.ZGraph();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // zGraph1
+            // 
+            this.zGraph1.BackColor = System.Drawing.Color.White;
+            this.zGraph1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.zGraph1.Location = new System.Drawing.Point(25, -7);
+            this.zGraph1.m_backColorH = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.zGraph1.m_backColorL = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_BigXYBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_BigXYButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_BigXYButtonForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_ControlButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_ControlButtonForeColorH = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.zGraph1.m_ControlButtonForeColorL = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.zGraph1.m_ControlItemBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_coordinateLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_coordinateStringColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_coordinateStringTitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_DirectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.zGraph1.m_DirectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_fXBeginSYS = 0F;
+            this.zGraph1.m_fXEndSYS = 60F;
+            this.zGraph1.m_fYBeginSYS = 0F;
+            this.zGraph1.m_fYEndSYS = 1F;
+            this.zGraph1.m_GraphBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_iLineShowColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_iLineShowColorAlpha = 100;
+            this.zGraph1.m_SySnameX = "X轴坐标";
+            this.zGraph1.m_SySnameY = "Y轴坐标";
+            this.zGraph1.m_SyStitle = "波形显示";
+            this.zGraph1.m_titleBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.zGraph1.m_titleColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_titlePosition = 0.4F;
+            this.zGraph1.m_titleSize = 14;
+            this.zGraph1.Margin = new System.Windows.Forms.Padding(0);
+            this.zGraph1.MinimumSize = new System.Drawing.Size(390, 270);
+            this.zGraph1.Name = "zGraph1";
+            this.zGraph1.Size = new System.Drawing.Size(708, 476);
+            this.zGraph1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(668, 505);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(499, 507);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(758, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(552, 473);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1412, 605);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zGraph1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZhengJuyin.UI.ZGraph zGraph1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WaveShape/WaveShape/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Threading;
+
+namespace WaveShape
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        //#region **测试数据**
+        //public List<float> x1 = new List<float>();
+        //public List<float> y1 = new List<float>();
+        //public List<float> x2 = new List<float>();
+        //public List<float> y2 = new List<float>();
+        //public List<float> x3 = new List<float>();
+        //public List<float> y3 = new List<float>();
+        //public List<float> x4 = new List<float>();
+        //public List<float> y4 = new List<float>();
+
+        //public List<float> y = new List<float>();
+        //int current = 200;
+
+
+        //#endregion
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    ///模拟串口采样显示[周期k]
+            
+        //    //if (int.TryParse(textBox1.Text.ToString(), out current))
+        //    //{
+        //    //    if (current > 100 && current < 300)
+        //    //    {
+        //    //        timer1.Interval = current;
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        textBox1.Text = "400";
+        //    //    }
+        //    //}
+        //    //else
+        //    //{
+        //    //    textBox1.Text = "200";
+        //    //}
+        //    //x1.Clear();
+        //    //y1.Clear();
+        //    //x2.Clear();
+        //    //y2.Clear();
+        //    //x3.Clear();
+        //    //y3.Clear();
+        //    //x4.Clear();
+        //    //y4.Clear();
+        //    //zGraph1.f_ClearAllPix();
+        //    //zGraph1.f_reXY();
+        //    //zGraph1.f_LoadOnePix(ref x1, ref y1, Color.Red, 2);
+            
+        //    //zGraph1.f_AddPix(ref x3, ref y3, Color.FromArgb(0, 128, 192), 2);
+        //    //zGraph1.f_AddPix(ref x4, ref y4, Color.Yellow, 3);
+        //    //zGraph1.f_Refresh();
+
+        //    //开始TIMER
+        //    //timerDrawI = 0;
+        //    //timer1.Start();
+
+        //    LoadDataList();
+        //}
+        //private int timerDrawI = 0;
+        //private void timer1_Tick(object sender, EventArgs e)
+        //{
+        //    ///TIME增加数据
+
+        //    x2.Add(timerDrawI);
+        //    y2.Add((float)Math.Sin(timerDrawI / 10f) * 200);
+
+        //    timerDrawI++;
+        //    zGraph1.f_Refresh();
+        //    //更新按钮显示，表示为正在采样
+        //    button1.Text += ".";
+        //    if (button1.Text.Length > 22)
+        //    {
+        //        button1.Text = "采样测试 正在采样.";
+        //    }
+        //    //LoadDataList();
+        //}
+        //public void LoadDataList()
+        //{
+        //    string sql = "SELECT s,x,y,z FROM datatest limit 20";
+        //    MySqlConnection con = null;
+        //    MySqlDataReader reader = null;
+        //    try
+        //    {
+                
+        //        con = new MySqlConnection("server=localhost;user id = root;database=frdtest;password=az13132323251..");
+        //        con.Open();
+        //        MySqlCommand cmd = new MySqlCommand(sql, con);
+        //        reader = cmd.ExecuteReader();
+        //        #region
+        //        while (reader.Read())
+        //        {
+        //            string s0 = reader[0].ToString();
+        //            string x0 = reader[1].ToString();
+        //            string y0 = reader[2].ToString();
+        //            string z0 = reader[3].ToString();
+
+        //            if (s0 != "")
+        //            {
+        //                float s = float.Parse(s0);
+        //                x2.Add(s);
+        //                Log.Write("s:"+s);
+        //            }
+        //            if (x0 != "")
+        //            {
+        //                float x = float.Parse(x0);
+        //                y2.Add(x);
+        //                Log.Write("x:" + x);
+        //            }
+        //            if (y0 != "")
+        //            {
+        //                float y = float.Parse(y0);
+        //            }
+        //            if (z0 != "")
+        //            {
+        //                float z = float.Parse(z0);
+        //            }
+        //            zGraph1.f_ClearAllPix();
+        //            zGraph1.f_reXY();
+        //            //zGraph1.f_LoadOnePix(ref x2, ref y2, Color.Red, 2);
+        //            zGraph1.f_AddPix(ref x2, ref y2, Color.Blue, 3);
+        //            zGraph1.f_Refresh();
+        //            //Thread.Sleep(200);
+        //        }
+        //        #endregion
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("异常：" + ex.Message);
+        //    }
+        //    finally
+        //    {
+        //        con.Close();
+        //    }
+        //}
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+        //    //LoadDataList();
+
+        //    string sql = "SELECT s,x,y,z FROM datatest";
+        //    MySqlConnection con = null;
+        //    MySqlDataAdapter adapter = null;
+        //    try
+        //    {
+        //        con = new MySqlConnection("server=localhost;user id = root;database=frdtest;password=az13132323251..");
+        //        con.Open();
+        //        MySqlCommand cmd = new MySqlCommand(sql, con);
+        //        DataSet ds = new DataSet();
+        //        adapter = new MySqlDataAdapter(cmd);
+        //        adapter.Fill(ds);
+        //        dataGridView1.DataSource = ds.Tables[0];
+        //        Log.Write("启动");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("异常：" + ex.Message);
+        //    }
+        //    finally
+        //    {
+        //        con.Close();
+        //    }
+        //}
+    }
+}
